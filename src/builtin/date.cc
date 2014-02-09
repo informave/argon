11c6ed182206cc5293d3e1f3f9df1db6afce3d41
Daniel Vogelbacher <daniel@vogelbacher.name>
2014-02-09 21:31:00 +0100

    Added more date functions
    date.diff()
    date.now()
    date.cast()
    date.add()
    date.sub()

--- src/builtin/date.cc
+++ src/builtin/date.cc
@@ -4,6 +4,9 @@
 
 #include <iterator>
 
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/date_generators.hpp>
+
 ARGON_NAMESPACE_BEGIN
 
 namespace date
@@ -48,7 +51,7 @@ namespace date
         year = m_args[0]->_value().data().get<int>();
         month = m_args[1]->_value().data().get<int>();
         day = m_args[2]->_value().data().get<int>();
-           
+
         return Value(Variant(TDate(year, month, day)));
     }
 
@@ -84,7 +87,7 @@ namespace date
 
 
 
-	struct no_separator : std::numpunct<char>
+    struct no_separator : std::numpunct<char>
     {
     protected:
         virtual string_type do_grouping() const
@@ -93,10 +96,10 @@ namespace date
 
     static std::string fmt_int(int val, size_t max_len, char fill)
     {
-		std::locale loc(std::locale("C"), new no_separator());
-        
+        std::locale loc(std::locale("C"), new no_separator());
+
         std::string res = Variant(val).asStr(loc);
-        
+
         if(res.size() > max_len)
             return res.substr(res.size()-2);
         else if(res.size() < max_len && fill != '\0')
@@ -160,9 +163,124 @@ namespace date
             ++i;
         }
 
-    return Value(String(res, "US-ASCII"));
+        return Value(String(res, "US-ASCII"));
     }
 
+
+    ARGON_FUNCTION_DEF(diff)
+    {
+        using namespace boost::gregorian;
+
+        ARGON_ICERR(m_args.size() == 3 , "invalid args count");
+
+        String typ = m_args[0]->_value().data().get<String>();
+
+        if(m_args[1]->_value().data().isnull() ||
+           m_args[2]->_value().data().isnull())
+            return Value();
+
+        TDate tmp1 = m_args[1]->_value().data().get<TDate>();
+        TDate tmp2 = m_args[2]->_value().data().get<TDate>();
+
+
+
+        boost::gregorian::date d1(tmp1.year(), tmp1.month(), tmp1.day() );
+        boost::gregorian::date d2(tmp2.year(), tmp2.month(), tmp2.day() );
+
+        if(typ == "year")
+        {
+            if (d1 > d2)
+                std::swap(d1, d2); // guarantee that d2 >= d1
+
+            boost::gregorian::partial_date pd1(d1.day(), d1.month());
+            boost::gregorian::partial_date pd2(d2.day(), d2.month());
+
+            int fullYearsInBetween = d2.year() - d1.year();
+            if (pd2 < pd1)
+                fullYearsInBetween -= 1;
+            return Value(fullYearsInBetween);
+        }
+
+        return Value(0); /// @bug FIXME
+    }
+
+
+    ARGON_FUNCTION_DEF(now)
+    {
+        ARGON_ICERR(m_args.size() == 0 , "invalid args count");
+
+        return Value(TDate("now"));
+    }
+
+
+
+    ARGON_FUNCTION_DEF(cast)
+    {
+        ARGON_ICERR(m_args.size() == 1 , "invalid args count");
+
+        if(m_args[0]->_value().data().isnull())
+            return Value();
+
+        return Variant(m_args[0]->_value().data().get<TDate>());
+    }
+
+
+
+
+    ARGON_FUNCTION_DEF(add)
+    {
+        using namespace boost::gregorian;
+
+        ARGON_ICERR(m_args.size() == 3 , "invalid args count");
+
+        String typ = m_args[1]->_value().data().get<String>();
+
+        if(m_args[0]->_value().data().isnull() ||
+           m_args[2]->_value().data().isnull())
+            return Value();
+
+        TDate tmp1 = m_args[0]->_value().data().get<TDate>();
+        int tmp2 = m_args[2]->_value().data().get<int>();
+
+        boost::gregorian::date d1(tmp1.year(), tmp1.month(), tmp1.day() );
+
+        if(typ == "years")
+            d1 = d1 + years(tmp2);
+        else if(typ == "months")
+            d1 = d1 + months(tmp2);
+        else if(typ == "days")
+            d1 = d1 + days(tmp2);
+
+        return Value(TDate(d1.year(), d1.month(), d1.day()));
+    }
+
+
+    ARGON_FUNCTION_DEF(sub)
+    {
+        using namespace boost::gregorian;
+
+        ARGON_ICERR(m_args.size() == 3 , "invalid args count");
+
+        String typ = m_args[1]->_value().data().get<String>();
+
+        if(m_args[0]->_value().data().isnull() ||
+           m_args[2]->_value().data().isnull())
+            return Value();
+
+        TDate tmp1 = m_args[0]->_value().data().get<TDate>();
+        int tmp2 = m_args[2]->_value().data().get<int>();
+
+        boost::gregorian::date d1(tmp1.year(), tmp1.month(), tmp1.day() );
+
+        if(typ == "years")
+            d1 = d1 - years(tmp2);
+        else if(typ == "months")
+            d1 = d1 - months(tmp2);
+        else if(typ == "days")
+            d1 = d1 - days(tmp2);
+
+        return Value(TDate(d1.year(), d1.month(), d1.day()));
+    }
 
 }
 
@@ -177,13 +295,21 @@ static Function* factory_function(Processor &proc, const ArgumentList &args)
 
 builtin_func_def table_date_funcs[] =
 {
-    { "debug2",		     factory_function<date::func_debug1>,	   1,  1 },
+    { "debug2",          factory_function<date::func_debug1>,      1,  1 },
 
-    { "date.encode",		 factory_function<date::func_encode>,	   3,  3 },
-    { "date.year",		     factory_function<date::func_year>,	       0,  0 },
-    { "date.month",		     factory_function<date::func_month>,	   0,  0 },
-    { "date.day",		     factory_function<date::func_day>,	       0,  0 },
-    { "date.format",		 factory_function<date::func_format>,	   2,  2 },
+    { "date.encode",         factory_function<date::func_encode>,      3,  3 },
+    { "date.year",           factory_function<date::func_year>,        0,  0 },
+    { "date.month",          factory_function<date::func_month>,       0,  0 },
+    { "date.day",            factory_function<date::func_day>,         0,  0 },
+    { "date.format",         factory_function<date::func_format>,      2,  2 },
+    { "date.diff",           factory_function<date::func_diff>,        3,  3 },
+
+    { "date.now",            factory_function<date::func_now>,         0,  0 },
+    { "date.cast",           factory_function<date::func_cast>,        1,  1 },
+
+    { "date.add",            factory_function<date::func_add>,         3,  3 },
+    { "date.sub",            factory_function<date::func_sub>,         3,  3 },
+
 
     { NULL, NULL, 0, 0 }
 };
